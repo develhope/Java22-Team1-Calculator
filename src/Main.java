15d83a6732e19effecc6c1618a4c2960870a84fb
AlessioStrafo <straforini.alessio@gmail.com>
2024-01-18 12:57:03 +0100

    add vari case

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Programma calcolatrice: ");
+        System.out.println("Programma Calcolatrice: ");
+        System.out.println("Scegli l' operazione da effettuare:");
+        System.out.println("Premi 1 per la somma");
+        System.out.println("Premi 2 per la sottrazione");
+        System.out.println("Premi 3 per la divisione");
+        System.out.println("Premi 4 per la moltiplicazione");
+        System.out.println("Premi 5 per sapere se sono numeri pari o dipari:");
+        System.out.println("Premi 6 per elevare il primo numero alla potenza del secondo:");
+        int scelta = sc.nextInt();
 
         System.out.println("Inserisci il primo numero:");
         double x=sc.nextDouble();
@@ -12,19 +20,26 @@ public class Main {
         System.out.println("Inserisci il secondo numero");
         double y=sc.nextDouble();
 
-        System.out.println("Scegli l' operazione da effettuare:");
-        System.out.println("Premi 1 per la somma");
-        System.out.println("Premi 2 per la sottrazione");
-        System.out.println("Premi 3 per la divisione");
-        System.out.println("Premi 4 per la moltiplicazione");
-        System.out.println("Premi 5 per sapere se è un numero pari o dipari:");
-        int scelta = sc.nextInt();
+
         switch (scelta){
             case 1:
                 System.out.println("La somma dei numeri: " + x + " + " + y + " = " + somma(x,y));
                 break;
             case 2:
-
+                System.out.println("La sottrazione dei numeri: " + x + " - " + y + " = " + (x,y));
+                break;
+            case 3:
+                System.out.println("La divisione dei numeri: " + x + " / " + y + " = " + (x,y));
+                break;
+            case 4:
+                System.out.println("La moltiplicazione dei numeri: " + x + " * " + y + " = " + (x,y));
+                break;
+            case 5:
+                System.out.println("I numeri sono: ");
+                break;
+            case 6:
+                System.out.println("L' elevazione alla potenza dei numeri: " + x + " di " + y + " = " +(x,y));
+                break;
         }
     }
     public static double somma(double x, double y){
