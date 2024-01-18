4ce9ae022ddbd2ad9fbe48adc35968f6011e864d
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-01-18 11:12:44 +0100

    added subtraction method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int num = 10;
 
+
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
     }
 
 }
